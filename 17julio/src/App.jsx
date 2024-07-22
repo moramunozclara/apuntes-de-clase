c74116ccac0f19248beacd938da90232784ab755
moramunozclara <moramunozclara@alumnos.cei.es>
2024-07-22 10:56:25 +0200

    fix

--- 17julio/src/App.jsx
+++ 17julio/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 
 import './App.css'
+import Personajes from './components/Personajes'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
+
+    <h1>Rick & Morty</h1>
+   <Personajes/>
+    
 
     </>
   )
--- 17julio/src/components/Personajes.jsx
+++ 17julio/src/components/Personajes.jsx
@@ -1,20 +1,46 @@
-import React {useState, useEffect} from "react";
+import {useState, useEffect} from "react";
 
 
 
 function Personajes() {
 
+// lista
+const [personajes, setPersonajes] = useState([]);
 
-const [personajes, setPersonajes] = useState();
+// objeto
+const [info, setInfo] = useState({});
 
 useEffect(() => {
     fetch('https://rickandmortyapi.com/api/character/')
-    .then(response => response.json )
-    .then(data => data.results)
+    .then(response => response.json() )
+    .then(data => {
+        console.log("Los datos de personaje son: ", data)
+            setPersonajes(data.results);
+            setInfo(data.info)
+    }
+)
 }, []);
 
 
-    return (  );
+    return ( 
+        <>
+        <h2>2Personajes</h2>
+
+        {personajes.map(function (personaje) {
+            return (
+                <>
+                <div>
+
+                    key={personaje.name}
+
+                    </div>
+                </>
+            );
+        })}
+        </>
+
+
+     );
 }
 
 export default Personajes;
