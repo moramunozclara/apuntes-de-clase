065560a2ae642c834c1597ec734cdb6e404a6e7d
Clara Mora <moramunozclara@alumnos.cei.es>
2024-07-23 23:09:33 +0200

    creacion pokeapi

--- asincronos/01_promesas/src/App.jsx
+++ asincronos/01_promesas/src/App.jsx
@@ -1,27 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './css/App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+        <>
+          <h1>Ejercicio 1. Promesas</h1>
+        </>
   )
+
 }
 
 export default App
---
+++ asincronos/01_promesas/src/components/Pokeapi.jsx
@@ -0,0 +1,73 @@
+import { useState, useEffect } from 'react'
+import './css/Pokeapi.css'
+
+
+const Pokeapi = () => {
+
+    const [pokeapi, setPokeapi] = useState({
+ 
+    });
+
+    const [error, setError] = useState("");
+
+    const [info, setInfo] = useState({
+        "count": 0,
+        "next": "null",
+        "previous": null,
+        "results": 0,
+    });
+
+
+    useEffect(() => {
+        getPokeapi('https://pokeapi.co/api/v2/pokemon');
+        console.log("Encontré lo siguiente: ");
+    }, []);
+
+    const getPokeapi = async (url) => {
+        const respuesta = await fetch(url);
+        const objeto = await respuesta.json;
+        console.log("Pokeapi.jsx objeto vale :", objeto);
+
+        if(objeto.error) {
+            setError("Hubo un error");
+            setPokeapi([]); //corchetes porque es array
+            setInfo({}); //llaves porque es objeto
+        }
+        else {
+            setError(''); //nada porque no hubo error
+            setPokeapi(objeto.results);
+            setInfo(objeto.info);
+
+        }
+
+    }
+
+    return (
+
+        // ETIQUETA VACÍA SIEMPRE, Y DENTRO TODO 
+        <>
+
+            {personajes.map(function (personaje) {
+                        return (
+                            <div key={personaje.id}>
+                                {personaje.id}{'. '}
+                                {'Name:'} {personaje.name} 
+                                {' '} 
+                                {'Gender:'} {personaje.gender}
+                                <img src={personaje.image} alt={personaje.name} />
+                                </div>
+                        );
+                    })}
+        
+        
+        
+        
+        
+        
+        </>
+        
+
+     );
+}
+ 
+export default Pokeapi;
