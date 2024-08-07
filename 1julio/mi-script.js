09116d764e1e8d090b1c1ee63f36e15b7b55aa2b
Clara Mora <moramunozclara@alumnos.cei.es>
2024-08-07 13:56:42 +0200

    fix

--- 1julio/mi-script.js
+++ 1julio/mi-script.js
@@ -15,7 +15,7 @@ const artGallery = [
 console.clear();
 
 // Ejercicio 1: forEach y template strings
-console.log("///Ejercicio 1: forEach y template strings");
+console.log("\n\n Ejercicio 1: forEach y template strings \n");
 artGallery.forEach(obra => {
   const esExhibida = obra.isExhibited ? "se encuentra exhibida" : "no se encuentra exhibida";
   // let isExhibited = "NO SE";
@@ -28,7 +28,7 @@ artGallery.forEach(obra => {
 
 
 // Ejercicio 2: MAP para mostrar solo los títulos en la consola
-console.log("///Ejercicio 2: MAP para mostrar solo los títulos en la consola");
+console.log("\n\n Ejercicio 2: MAP para mostrar solo los títulos en la consola \n");
 
 const listaTitulo = artGallery.map (obra => obra.title);
   console.log(listaTitulo);
@@ -36,7 +36,7 @@ const listaTitulo = artGallery.map (obra => obra.title);
 
 
 // Ejercicio 3: FILTER en Exhibición
-console.log("///Ejercicio 3: FILTER en Exhibición");
+console.log("\n\n Ejercicio 3: FILTER en Exhibición \n");
 
 const listaEnExhibicion = artGallery.filter(obra => obra.isExhibited === true);
   console.log(listaEnExhibicion);
@@ -44,15 +44,23 @@ const listaEnExhibicion = artGallery.filter(obra => obra.isExhibited === true);
 
 
 // Ejercicio 4: FIND, solo devuelve un titulo de obra, con "The Night Watch"
-console.log("///Ejercicio 4: FIND, solo devuelve un titulo de obra, con 'The Night Watch'");
+console.log("\n\n Ejercicio 4: FIND, solo devuelve un titulo de obra, con 'The Night Watch' \n");
 
-const rondaDeNoche = artGallery.find(obra => obra.title == "The Night Watch");
-  console.log(rondaDeNoche);
+const encontrarPorTitulo = (title) => {
+
+  // pasar el título a minúsculas y quitar espacios al principio y final
+  title = title.toLowerCase().trim();
+
+  const resultado = artGallery.find(obra => obra.title.toLowerCase().trim() === title);
+  if (!resultado) { return "Obra no encontrada"} return resultado
+}
+  console.log("Buscando The Night Watch:", encontrarPorTitulo("The Night Watch"));
+  console.log("Buscando Ecce Homo:", encontrarPorTitulo("Ecce Homo"));
 
 
 
 // Ejercicio 5: SOME (verifica si hay o no con true o false), con 1503
-console.log("///Ejercicio 5: SOME (verifica si hay o no con true o false), con 1503");
+console.log("\n\n Ejercicio 5: SOME (verifica si hay o no con true o false), con 1503 ");
 const obraExampleYear = artGallery.some(obra => obra.year == 1503)
 const mensajeExtra = obraExampleYear ? "Se han encontrado obras de 1503" : "No se han encontrado obras de 1503";
   console.log(obraExampleYear, mensajeExtra);
@@ -60,7 +68,7 @@ const mensajeExtra = obraExampleYear ? "Se han encontrado obras de 1503" : "No s
 
 
 // Ejercicio 6: EVERY ()
-console.log("///Ejercicio 6: EVERY() todas las obras anteriores a mi año de nacimiento: 1998");
+console.log("\n\n Ejercicio 6: EVERY() todas las obras anteriores a mi año de nacimiento: 1998");
 
 const obraMyBirthDate = artGallery.every(obra => obra.year < 1998);
 const mensajitoExtra = obraMyBirthDate ? "todas las obras son previas a mi nacimiento" : "hay obras posteriores a mi nacimiento"
@@ -69,7 +77,7 @@ const mensajitoExtra = obraMyBirthDate ? "todas las obras son previas a mi nacim
 
 
 // Ejercicio 7: findIndex() de la obra "Mona Lisa"
-console.log("///Ejercicio 7: findIndex() posición en el array de la obra 'Mona Lisa'");
+console.log("\n\n Ejercicio 7: findIndex() posición en el array de la obra 'Mona Lisa'");
 
 const posicionObra = artGallery.findIndex(obra => obra.title == "Mona Lisa");
   console.log(`La Mona Lisa está en la posición ${posicionObra+1} `);
@@ -85,7 +93,7 @@ const posicionObra = artGallery.findIndex(obra => obra.title == "Mona Lisa");
 // Ejercicio 9: Agregar una función que permita cambiar el estado de isExhibited de una obra
 //  enviándole el id
 // con find
-console.log("///Ejercicio 9: cambiar estado de exhibición con el id");
+console.log("\n\n Ejercicio 9: cambiar estado de exhibición con el id");
 
 const cambiarEstadoIsExhibited = (id) => {
   const obra = artGallery.find(obra => obra.id == 10);
