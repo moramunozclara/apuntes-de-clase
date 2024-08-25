3b284fe94a83dcf183d9636f3d5869382b66e145
Clara Mora <moramunozclara@alumnos.cei.es>
2024-08-25 14:58:13 +0200

    fix

--- 00_Actividades/03_Component_party/src/components/Restaurante.jsx
+++ 00_Actividades/03_Component_party/src/components/Restaurante.jsx
@@ -1,6 +1,38 @@
-const Restaurante = () => {
+const Restaurante = ({menu}) => {
+
+    const {entrantes, principales, postres} = menu;
+
     return ( 
-        <></>
+        <>
+        <div className="Card">
+            
+            <div>
+                <h2>Entrantes</h2>
+                <div>
+                    {entrantes.map(({id, nombre, precio, descripcion}) => (
+                        <div key={id}>
+                            <h3>{nombre}</h3>
+                            <h3>{precio}</h3>
+                            <p>{descripcion}</p>
+                        </div>
+                    ) )}
+            </div>
+        </div>
+
+        <div>
+        <h2>Principales</h2>
+        <h2></h2>
+        <h2></h2>
+        </div>
+
+        <div>
+        <h2>Postres</h2>
+        <h2></h2>
+        <h2></h2>
+        </div>
+
+        </div>
+        </>
      );
 }
  
