da231866db0b4530c28a8feb8fa96dd0558cf427
Clara Mora <moramunozclara@alumnos.cei.es>
2024-08-26 21:52:27 +0200

    fix

--- 00_Actividades/04_UseState/src/components/InterruptorOnOff.jsx
+++ 00_Actividades/04_UseState/src/components/InterruptorOnOff.jsx
@@ -5,16 +5,24 @@ const InterruptorOnOff = () => {
 
     const [interruptor, setInterruptor] = useState("Encendido");
 
-
     return ( 
         <div className={`Card ${interruptor === "Encendido" ? "CardEncendido" : "CardApagado"}`}>
 
             <p>La tarjeta está {interruptor === "Encendido" ? "encendida" : "apagada"}</p>
 
-            <button onClick={() => setInterruptor(interruptor === "Encendido" ? "Apagado" : "Encendido") }>
-                {interruptor === "Encendido" ? "Apagar" : "Encender"}</button>
+            <button 
+                onClick={() => {
+                    setInterruptor(interruptor === "Encendido" ? "Apagado" : "Encendido");
+                    console.log(`El interruptor está ${interruptor === "Encendido" ? "Apagado" : "Encendido"}`);
+                }}>
+                {interruptor === "Encendido" ? "Apagar" : "Encender"}
+            </button>
+                
         </div>
      );
+
+
 }
- 
+
+
 export default InterruptorOnOff;
--- 00_Actividades/04_UseState/src/components/ListaTareasBasica.jsx
+++ 00_Actividades/04_UseState/src/components/ListaTareasBasica.jsx
@@ -1,6 +1,24 @@
+import { useState } from "react";
+
+
 const ListaTareasBasica = () => {
+
+    const [tareas, setTarea] = useState("No hay tareas");
+
     return ( 
-        <div className="Card">
+        <div className="Card CardEncendido">
+            <p>Crea un componente que permita añadir tareas a una lista y mostrarlas.
+                </p>
+
+                <input type="text" />
+                <button
+                onClick={() => {setTarea}}>
+                    Añadir tarea
+                    </button>
+
+                <button>
+                    Borrar lista
+                    </button>
             
         </div>
      );
