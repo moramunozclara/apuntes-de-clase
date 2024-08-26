ab6a1f5ac55840d62877eaa2303ea0a1d7f635a0
Clara Mora <moramunozclara@alumnos.cei.es>
2024-08-26 22:59:42 +0200

    fix

--- 00_Actividades/04_UseState/src/components/ListaTareasBasica.jsx
+++ 00_Actividades/04_UseState/src/components/ListaTareasBasica.jsx
@@ -3,22 +3,49 @@ import { useState } from "react";
 
 const ListaTareasBasica = () => {
 
-    const [tareas, setTarea] = useState("No hay tareas");
+    const [tareas, setTareas] = useState([]);
+    const [nuevaTarea, setNuevaTarea] = useState(""); // Estado para almacenar la nueva tarea
+
 
     return ( 
         <div className="Card CardEncendido">
             <p>Crea un componente que permita añadir tareas a una lista y mostrarlas.
                 </p>
 
-                <input type="text" />
-                <button
-                onClick={() => {setTarea}}>
-                    Añadir tarea
-                    </button>
+                <input 
+                    value={nuevaTarea} // El valor del input se vincula al estado
+                    // Cada vez que el usuario escribe algo, se actualiza el estado
+                    onChange={(e) => {          
+                        setNuevaTarea(e.target.value);
+                        console.log("[ListaTareasBasica] onChange", e.target.value);
+                    }}
+                    placeholder="Ingresar tarea" // Texto que aparece en el input cuando está vacío
+                />
 
-                <button>
+
+                <button
+                    onClick={() => {
+                        // si el texto del input no está vacío( !== "" )
+                        if (nuevaTarea !== "") {
+                            // añadir a las tareas existentes (...tareas), la nuevaTarea
+                            setTareas([...tareas, nuevaTarea]);
+                            // y después limpiar el input de entrada de tareas
+                            setNuevaTarea(""); 
+                        }
+                    }}>
+                    Añadir tarea
+                </button>
+
+                <button onClick={() => {setTareas([])}}>
                     Borrar lista
                     </button>
+
+                    <ul>
+                        {tareas.map((tarea, index) => (
+                            <li key={index}>{tarea}</li>
+                        ))}
+                        
+                    </ul>
             
         </div>
      );
