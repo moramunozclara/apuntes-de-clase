0eb5567ee1386368873dbc02f53716e974f8b355
Clara Mora <moramunozclara@alumnos.cei.es>
2024-08-30 13:22:42 +0200

    fix

--- 00_Actividades/04_UseState/src/components/FormularioRegistro.jsx
+++ 00_Actividades/04_UseState/src/components/FormularioRegistro.jsx
@@ -1,9 +1,40 @@
 import { useState } from "react";
 
 const FormularioRegistro = () => {
+
+    const [nombre, setNombre] = useState();
+
+    const [correo, setCorreo] = useState();
+
+
+
+
+
     return ( 
         <>
         <div className="Card">
+            <h2>Formulario de registro</h2>
+            <form action="">
+                <label htmlFor="name">Nombre </label>
+                <input 
+                name="nombre"
+                id="name"
+                type="text" 
+                value={nombre} 
+                onChange={(e) => {setNombre(e.target.value);
+                    console.log("Nombre:", e.target.value)}}
+                placeholder="Escriba su nombre"/>
+
+                <br />
+                <label htmlFor="email">Email </label>
+                <input id="email" 
+                name="correo"
+                type="email" 
+                value={correo} 
+                onChange={(e) => {setCorreo(e.target.value);
+                    console.log("Mail:", e.target.value)}} 
+                placeholder="Escriba su correo electrónico" />
+                </form>
             </div></>
      );
 }
