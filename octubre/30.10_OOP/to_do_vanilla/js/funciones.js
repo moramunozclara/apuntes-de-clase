95bd4d23cb4f55eb6f406622c54b6644b547e5c9
Clara Mora <moramunozclara@alumnos.cei.es>
2024-11-15 00:10:03 +0100

    connecting frontend to backend + db

--- octubre/30.10_OOP/to_do_vanilla/js/funciones.js
+++ octubre/30.10_OOP/to_do_vanilla/js/funciones.js
@@ -2,13 +2,42 @@ const formulario = document.querySelector("form");
 const inputText = document.querySelector('form input[type="text"]');
 const contenedorTareas = document.querySelector(".tareas");
 
+// carga inicial de datos almacenados en la DB
+fetch("http://localhost:4000/tareas")
+.then(respuesta => respuesta.json())
+.then(tareas => {
+    tareas.forEach(({id, title, isCompleted}) => {
+        new Tarea(id, title, isCompleted, contenedorTareas);
+    });
+});
 
 formulario.addEventListener("submit", evento => {
     evento.preventDefault();
 
     if(inputText.value.trim() != ""){ // si NO está vacío ( != "")
-        new Tarea(1,inputText.value.trim(),false,contenedorTareas); //crear nueva tarea
-        inputText.value = ""; // limpiar el input una vezse alade la tarea
+
+        // new Tarea(1,inputText.value.trim(),false,contenedorTareas); //crear nueva tarea
+        // inputText.value = ""; // limpiar el input una vez se agrega la tarea
+
+        const title = inputText.value.trim();
+
+        fetch("http://localhost:4000/tareas/nueva", {
+            method: "POST",
+            body: JSON.stringify({title}), // incluir input text value trim
+            headers: {
+                "Content-type" : "application/json"
+            }
+        })
+
+        .then(respuesta => respuesta.json())
+        // .then( x => { console.log(x); });
+        .then(({id, error}) => {
+            if (!error) {
+                    new Tarea(id, title, false, contenedorTareas);
+                    return inputText.value = "";
+            }
+            console.log("error en la peticion"); //error visible para el usuario
+        });
     }
     
 })
--- octubre/30.10_OOP/to_do_vanilla/js/tarea.js
+++ octubre/30.10_OOP/to_do_vanilla/js/tarea.js
@@ -2,11 +2,12 @@ class Tarea{
     constructor(id,texto,estado,contenedor){
         this.id = id;
         this.texto = texto;
-        this.editando = false;
+        this.editando = false; // ¿Estamos editando? esto por defecto es FALSE
         this.DOM = null;//el HTML de la tarea
 
         this.crearDOM(estado,contenedor);
     }
+
     crearDOM(estado,contenedor){
         //contenedor TAREA
         this.DOM = document.createElement("div");
@@ -22,9 +23,7 @@ class Tarea{
         editor.setAttribute("type","text");
         editor.value = this.texto;
 
-        //botón editar
-
-        // cuando presio editar, la linea de texto se vuelve visible
+        //botón editar // cuando presiono editar, la linea de texto se vuelve visible
         let botonEditar = document.createElement("button");
         botonEditar.classList.add("boton");
         botonEditar.innerText = "editar";
@@ -46,7 +45,8 @@ class Tarea{
 
         botonEstado.addEventListener("click", () => {
             this.editarEstado()
-            .then(() => botonEstado.classList.toggle("terminada"));
+            .then(() => botonEstado.classList.toggle("terminada"))
+            .catch(() => console.log("error cambiando el estado"));
         });
 
 
@@ -58,30 +58,85 @@ class Tarea{
         this.DOM.appendChild(botonEstado);
         contenedor.appendChild(this.DOM);
     }
+
     editarEstado(){
         return new Promise((ok,ko) => {
-            ok();
+  
+            fetch(`http://localhost:4000/tareas/actualizar/${this.id}/2`, {
+                method: "PUT"
+            })
+            .then(respuesta => respuesta.json())
+            .then(({resultado, error}) => {
+                if (error || resultado == "ko") {
+                    return ko();                    
+                }
+                ok();
+            })
+            .catch(ko);
         });
-    }
-    editarTexto(){
+    };
+    
+
+// editarTexto será una tarea ASYNC, porque hasta no  mandar
+// la tarea editada, no querremos que nos haga el cambio de guardar/editar
+    async editarTexto(){
         if(this.editando){
-            // console.log("guardar cambios")
+            // // console.log("guardar cambios")
+
+            let tareaTemporal = this.DOM.children[1].value.trim();
+            if (tareaTemporal != "" && tareaTemporal != this.texto) {
+
+                let {resultado, error} = await fetch(`http://localhost:4000/tareas/actualizar/${this.id}/1`, {
+                    method: "PUT",
+                    body: JSON.stringify({title : tareaTemporal}),
+                    headers: {
+                        "Content-type" : "application/json"
+                    }
+                }).then(respuesta => respuesta.json());
+            
+
+            if(error || resultado == "ko") {  // sería lo mismo que !error && resultado=="ok"
+                console.log("Error editando el texto de la tarea");
+
+            } else {
+                this.texto = tareaTemporal // una vez sabemos que se hicieron los cambios en la db,
+                                            // lo mostramos en el front
+            }
+        }
+            // console.log("empezar a editar")
             this.DOM.children[1].classList.remove("visible");
-            this.DOM.children[0].value = this.texto;
+            this.DOM.children[0].innerText = this.texto;
             this.DOM.children[0].classList.add("visible");
             this.DOM.children[2].innerText = "editar";
-        }else{
-            // console.log("empezar a editar")
+        } else {
+
             this.DOM.children[0].classList.remove("visible");
             this.DOM.children[1].value = this.texto;
             this.DOM.children[1].classList.add("visible");
-            this.DOM.children[0].innerText = "guardar";
-
+            this.DOM.children[2].innerText = "guardar";
 
         }
-        this.editando = !this.editando;
+            this.editando = !this.editando;
+
     }
+
+
     borrarTarea(){
-        this.DOM.remove();
+        fetch(`http://localhost:4000/tareas/borrar/${this.id}`,{
+            method : "DELETE"
+        })
+        .then(respuesta => respuesta.json())
+        .then(({resultado,error}) => {
+            if(error || resultado == "ko"){
+                return console.log("Error borrando tarea", error); // ko
+            }
+            this.DOM.remove();                              // ok
+        })
+        .catch(error => {
+            // Este bloque captura cualquier error en la solicitud fetch
+            console.error("Error en la solicitud:", error);
+        });
+        
     }
+
 }
